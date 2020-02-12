3438d2c58526cadbf103b10b59684eb7ea590437
sebastian eriksson <sebbe.eriksson09@icloud.com>
2020-02-12 14:21:48 +0100

    bläh

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import Join from "./components/Join/Join.js";
 import Chat from "./components/Chat/Chat.js";
 import Register from "./components/Register/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css"
 
 const App = () => {
@@ -16,16 +15,12 @@ const App = () => {
 
     return (
         <Router>
-
             <Route path="/" component={Login} />
-
             <Route path="/chat" component={Chat} />
             <Route path="/join" exact component={Join} />
             <Route path="/reg" exact component={Register} />
         </Router>
     )
 }
-
-
 
 export default App; 
